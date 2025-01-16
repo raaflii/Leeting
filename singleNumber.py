c4611d213c7266ed99fe7bde8ad8ef1033df61de
Muhammad Rafli <154863009+raaflii@users.noreply.github.com>
2025-01-16 14:28:40 +0700

    single number

---
+++ singleNumber.py
@@ -0,0 +1,12 @@
+def singleNumber(nums):
+    list = []
+    for i in nums:
+        if i not in list:
+            list.append(i)
+        else:
+            list.remove(i)
+    
+    return int("".join(map(str, list)))
+
+a = singleNumber([4,1,2,1,2])
+print(a)
