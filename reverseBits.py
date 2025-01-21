0f278b8a1a811369d232a715a60bfa179fb9f660
Muhammad Rafli <154863009+raaflii@users.noreply.github.com>
2025-01-21 12:54:35 +0700

    fix with input int reverse bits

--- reverseBits.py
+++ reverseBits.py
@@ -1,13 +1,20 @@
 def reverseBits(n):
+    n = format(n, 'b')
+
+    zero = ''
+    if len(n) != 32:
+        for i in range (32 - len(n)):
+            zero += '0'
+
+    n = zero + n
+
     reverse = str(n)[::-1]
-    print(reverse)
 
     res = 0
     for i in range (len(reverse) - 1, -1, -1):
-        print(i)
         res += int(reverse[abs(i - (len(reverse) - 1))]) * 2 ** i
 
     return res
 
-a = reverseBits("00000010100101000001111010011100")
+a = reverseBits(0b00000010100101000001111010011100)
 print(a)
