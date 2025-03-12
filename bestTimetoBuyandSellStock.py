1a550f514531a030a646c5e33ea53b826c6f3650
Muhammad Rafli <154863009+raaflii@users.noreply.github.com>
2025-03-12 23:57:27 +0700

    best time to buy adn sell stock

--- bestTimetoBuyandSellStock.py
+++ bestTimetoBuyandSellStock.py
@@ -1,13 +1,4 @@
 def maxProfit(prices):
-    # prob = []
-    # for i in range (len(prices)):
-    #     for j in range(i+1, len(prices)):
-    #         if prices[i] < prices[j]:
-    #             prob.append(prices[j] - prices[i])
-
-    # if len(prob) != 0: return max(prob)
-    # else: return 0
-    
     min_price = float('inf')
     max_profit = 0 
     
@@ -16,7 +7,6 @@ def maxProfit(prices):
         max_profit = max(max_profit, i - min_price)
     
     return max_profit
-        
 
 a = maxProfit([7,1,5,3,6,4])
 print(a)
